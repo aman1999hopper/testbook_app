2aa9cbed10433d10b03c13c8c4c0879bd9a048d8
aman bhardwaj <amanbhardwaj5931@gmail.com>
2024-09-03 16:48:32 +0530

    Change Styling for offerSection

--- frontend/src/components/OfferSection.jsx
+++ frontend/src/components/OfferSection.jsx
@@ -7,7 +7,7 @@ export const OfferSection = () => {
             <h1 class="font-bold text-3xl text-blue-950">What We Offer</h1>
         </div>
         <div class="grid sm:grid-cols-3 grid-cols-1 gap-4">
-            <div class="mt-10 border p-4 rounded-xl  hover:bg-blue-900 hover:text-white transform transition-transform duration-300 hover:scale-105">
+            <div class="mt-10 border p-4 rounded-xl  hover:bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white transform transition-transform duration-300 hover:scale-105">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="Pink" class="bi bi-book-half" viewBox="0 0 16 16">
         <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
@@ -17,7 +17,7 @@ export const OfferSection = () => {
 
             </div>
         </div>
-        <div class="mt-10 border p-4 rounded-xl hover:bg-blue-900 hover:text-white transform transition-transform duration-300 hover:scale-105">
+        <div class="mt-10 border p-4 rounded-xl hover:bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white transform transition-transform duration-300 hover:scale-105">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="Red" class="bi bi-question-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -29,7 +29,7 @@ export const OfferSection = () => {
             </div>
         </div>
 
-        <div class="mt-10 border  p-4 rounded-xl hover:bg-blue-900 hover:text-white transform transition-transform duration-300 hover:scale-105">
+        <div class="mt-10 border  p-4 rounded-xl hover:bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white transform transition-transform duration-300 hover:scale-105">
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="Red" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
